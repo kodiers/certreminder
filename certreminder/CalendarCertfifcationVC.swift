f7a1e4f402442d6aec0c875fc32a5bf5a331fb01
VY <kodiers@gmail.com>
2017-10-03 01:18:30 +0300

    added calendarvc

---
+++ certreminder/CalendarCertfifcationVC.swift
@@ -0,0 +1,35 @@
+//
+//  CalendarCertfifcationVC.swift
+//  certreminder
+//
+//  Created by Viktor Yamchinov on 03/10/2017.
+//  Copyright © 2017 Viktor Yamchinov. All rights reserved.
+//
+
+import UIKit
+
+class CalendarCertfifcationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
