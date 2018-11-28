1fef5af25ce6e0f2d751c20501148d811eb0846c
Victor Yamchinov <kodiers@gmail.com>
2018-11-29 02:15:14 +0300

    changed api for prod

--- certreminder/Constants.swift
+++ certreminder/Constants.swift
@@ -15,9 +15,9 @@ let MIN_PASSWORD_LENGTH = 9
 
 let APP_EMAIL = "support@techforline.com"
 let APP_SITE = "http://reminder.techforline.com"
-let APP_API_URL = "http://certapp.techforline.com"
+let APP_API_URL = "http://api-reminder.techforline.com"
 
-let CUSTOM_ERROR_DOMAIN = "certapp.techforline.com"
+let CUSTOM_ERROR_DOMAIN = "api-reminder.techforline.com"
 let ERROR_CODE_EXAM_EXISTS = 200
 let ERROR_CODE_LOGIN = 100
 let ERROR_CODE_HTTP_ERROR = 10
